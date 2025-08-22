d678f8e4c1f675725979cd1ebe1f950b35fd3e6c
awage <alexandre.wagemakers@gmail.com>
2025-08-22 12:38:29 +0200

    strange basins check

---
+++ scripts/check_weird_basins.jl
@@ -0,0 +1,43 @@
+using DrWatson
+@quickactivate 
+using CairoMakie
+using LaTeXStrings
+using Attractors
+using ProgressMeter
+
+include(srcdir("model_mapper.jl"))
+include(srcdir("bifur_diag.jl"))
+
+force = false
+σ = 0.3; ω = 1.0; μ = 35.0; η = 0.08; δ = 1.0; β = 0.4
+dps = model_parameters(ω, σ, β, η, μ, δ)
+res = 500
+yg = range(-5,5, length=res); grid = (yg, yg) 
+Np = 100; 
+δrange = range(-30, 30, length = Np)
+σrange = range(0.1, 0.5, length = Np)
+
+Sb = zeros(Np, Np)
+Sbb = zeros(Np, Np)
+@showprogress for j in eachindex(δrange) 
+    for k in eachindex(σrange)
+        data = get_basins(dps, grid; force, show_progress = true)
+        @unpack bas = data
+        # Sb[j,k], Sbb[j,k] = basin_entropy(bas, 10) 
+        if length(unique(Na)) > 5
+            @show δrange[j], σrange[k]
+            f = Figure(size = (800,800))
+            ax = Axis(f[1,1], xlabel = L"x_1", ylabel = L"x_2") #, yscale = log10);
+            heatmap!(ax, xg, yg, bsn; rasterize = true)
+            params = (σ,  ω,  μ,  η,  δ,  β)
+            save(savename("weird",params, "pdf"),f)
+        end
+    end
+end
+
+# fig = Figure(size = (600,600))
+# ax = Axis(fig[1,1], ylabel = L"\delta", xlabel = L"\sigma",  yticklabelsize = 10, xticklabelsvisible = false, ylabelsize = 15, xlabelsize = 15)
+# heatmap!(ax, δrange, σrange, Sb; rasterize = true)
+# save("2d_basin_entropy.pdf", fig)
+
+
